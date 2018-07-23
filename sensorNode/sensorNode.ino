7a981461d6ff75faf392b4513eed75182813a269
Lara Timm <l_timm@hotmail.com>
2018-07-23 23:15:21 +0200

    Added structs and checkOccupied function

--- sensorNode/sensorNode.ino
+++ sensorNode/sensorNode.ino
@@ -1,21 +1,29 @@
 #include <avr/power.h>
+#include <string.h>
 
 const int trig1 = 2; 
 const int trig2 = 3; 
 const int trig3 = 4; 
 const int trig4 = 5; 
-
 const int echo1 = 6;
 const int echo2 = 7;
 const int echo3 = 8;
 const int echo4 = 9;
 
-const float wait = 0.1;
+const float wait = 500;
+float timeoutDist;
+float timeout;
+
+struct bayInfo {
+  const char bayName[];
+  int occupancyStatus;
+}; 
 
  
 void setup() {
   clock_prescale_set(clock_div_16);
   Serial.begin (9600);
+  
   pinMode(trig1, OUTPUT);
   pinMode(trig2, OUTPUT);
   pinMode(trig3, OUTPUT);
@@ -24,8 +32,14 @@ void setup() {
   pinMode(echo2, INPUT);
   pinMode(echo3, INPUT);
   pinMode(echo4, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+
+  timeoutDist = 100; //100cm
+  timeout = timeoutDist * 58; //microSeconds
+
+  bayInfo bay1 = {"bay001", 0};
+  bayInfo bay2 = {"bay002", 0};
+  bayInfo bay3 = {"bay003", 0};
+  bayInfo bay4 = {"bay004", 0};
 }
  
 void loop() {
@@ -33,28 +47,34 @@ void loop() {
   float distance1, distance2, distance3, distance4;
 
   distance1 = getDistance(trig1, echo1);
+  bay1.occupancyStatus = checkOccupied(distance1);
   Serial.print("Distance1: ");
   Serial.print(distance1);
-  Serial.println(" cm");
-  //delay(wait);
+  Serial.print(" cm \t");
+  Serial.print("status:")
+  Serial.println(bay1.occupancyStatus);
+  delay(wait);
 
   distance2 = getDistance(trig2, echo2);
+  bay2.occupancyStatus = checkOccupied(distance2);
   Serial.print("Distance2: ");
   Serial.print(distance2);
   Serial.println(" cm");
-  //delay(wait);
+  delay(wait);
 
   distance3 = getDistance(trig3, echo3);
+  bay3.occupancyStatus = checkOccupied(distance3);
   Serial.print("Distance3: ");
   Serial.print(distance3);
   Serial.println(" cm");
-  //delay(wait);
+  delay(wait);
 
   distance4 = getDistance(trig4, echo4);
+  bay4.occupancyStatus = checkOccupied(distance4);
   Serial.print("Distance4: ");
   Serial.print(distance4);
   Serial.println(" cm\n");
-  //delay(wait);
+  delay(wait);
 }
 
 float getDistance(const int trigPin, const int echoPin){
@@ -64,8 +84,20 @@ float getDistance(const int trigPin, const int echoPin){
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
+  duration = pulseIn(echoPin, HIGH, timeout);
+  distance = duration/58;
   return distance;
 }
+
+int checkOccupied(float distance) {
+  int occupanceStatus;
+  if (distance < 0 && distance <= 100){
+    occupanceStatus = 1;
+  } else {
+    occupanceStatus = 0;
+  }
+  return occupanceStatus;
+}
+
+
 
