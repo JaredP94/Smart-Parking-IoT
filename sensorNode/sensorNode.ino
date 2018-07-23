d2bb60c9a9daf4ea79404224058fe6e51d570360
Lara Timm <l_timm@hotmail.com>
2018-07-23 20:47:02 +0200

    getDistance function added

--- sensorNode/sensorNode.ino
+++ sensorNode/sensorNode.ino
@@ -31,61 +31,41 @@ void setup() {
 void loop() {
  
   float distance1, distance2, distance3, distance4;
-  float duration1, duration2, duration3, duration4;
-  
-  digitalWrite(trig1, LOW);
-  delayMicroseconds(2); 
-  digitalWrite(trig1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig1, LOW);
-  duration1 = pulseIn(echo1, HIGH);
-  distance1 = (duration1/2) / 29.1;
+
+  distance1 = getDistance(trig1, echo1);
   Serial.print("Distance1: ");
   Serial.print(distance1);
   Serial.println(" cm");
   //delay(wait);
-  
-  digitalWrite(trig2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig2, HIGH);
-  delayMicroseconds(10); 
-  digitalWrite(trig2, LOW);
-  duration2 = pulseIn(echo2, HIGH);
-  distance2 = (duration2/2) / 29.1;
+
+  distance2 = getDistance(trig2, echo2);
   Serial.print("Distance2: ");
   Serial.print(distance2);
   Serial.println(" cm");
   //delay(wait);
-  
-  digitalWrite(trig3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig3, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig3, LOW);
-  duration3 = pulseIn(echo3, HIGH);
-  distance3 = (duration3/2) / 29.1;
+
+  distance3 = getDistance(trig3, echo3);
   Serial.print("Distance3: ");
   Serial.print(distance3);
   Serial.println(" cm");
   //delay(wait);
 
-  digitalWrite(trig4, LOW);
-  delayMicroseconds(2); 
-  digitalWrite(trig4, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig4, LOW);
-  duration4 = pulseIn(echo4, HIGH);
-  distance4 = (duration4/2) / 29.1;
+  distance4 = getDistance(trig4, echo4);
   Serial.print("Distance4: ");
   Serial.print(distance4);
   Serial.println(" cm\n");
   //delay(wait);
 }
 
-//float getDistance(const int pin){
-//  float distance, duration;
-//  duration = pulseIn(pin, HIGH);
-//  distance = (duration/2) / 29.1;
-//  return distance;
-//}
+float getDistance(const int trigPin, const int echoPin){
+  float distance, duration;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  return distance;
+}
 
