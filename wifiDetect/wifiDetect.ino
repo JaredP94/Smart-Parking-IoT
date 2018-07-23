45d098b9dcbf19b02b87eeb96692e49c1da01ce8
Jared <jaredping@yahoo.com>
2018-07-23 14:18:38 +0200

    Network detector

---
+++ wifiDetect/wifiDetect.ino
@@ -0,0 +1,27 @@
+#include "ESP8266WiFi.h"
+
+void setup() { 
+  Serial.begin(9600);
+} 
+
+void loop() { 
+
+  Serial.println("\n\n\n\n\n\n\n\n\n\n");
+  
+  int numberOfNetworks = WiFi.scanNetworks();
+ 
+  for(int i =0; i<numberOfNetworks; i++){
+ 
+      Serial.print("Network name: ");
+      Serial.println(WiFi.SSID(i));
+      Serial.print("Signal strength: ");
+      Serial.println(WiFi.RSSI(i));
+      Serial.println("-----------------------");
+ 
+  }
+
+  delay(5000);
+}
+
+
+
