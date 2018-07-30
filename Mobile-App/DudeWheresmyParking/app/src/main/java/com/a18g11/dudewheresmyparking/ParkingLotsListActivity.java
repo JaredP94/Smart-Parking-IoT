5add758eeec010ae9b749bb5b0936a8a24e6024a
Jared <jaredping@yahoo.com>
2018-07-30 17:45:00 +0200

    Base application + Splash screen

---
+++ Mobile-App/DudeWheresmyParking/app/src/main/java/com/a18g11/dudewheresmyparking/ParkingLotsListActivity.java
@@ -0,0 +1,30 @@
+package com.a18g11.dudewheresmyparking;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.group18g11.dudewheresmyparking.R;
+
+public class ParkingLotsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_lots_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
