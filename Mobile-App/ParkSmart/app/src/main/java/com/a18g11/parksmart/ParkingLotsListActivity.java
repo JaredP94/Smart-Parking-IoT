1df908ca6b2671c33b8c029a8ff4111c77d275e0
Jared <jaredping@yahoo.com>
2018-07-31 18:28:14 +0200

    Parking lot list activity + UI

--- Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLotsListActivity.java
+++ Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLotsListActivity.java
@@ -1,30 +1,47 @@
 package com.a18g11.parksmart;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.group18g11.parksmart.R;
 
-public class ParkingLotsListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ParkingLotsListActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lots_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Parking Lot 1 \t Value");
+        animalNames.add("Parking Lot 2 \t Value");
+        animalNames.add("Parking Lot 3 \t Value");
+        animalNames.add("Parking Lot 4 \t Value");
+        animalNames.add("Parking Lot 5 \t Value");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.parking_lots);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(decoration);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
