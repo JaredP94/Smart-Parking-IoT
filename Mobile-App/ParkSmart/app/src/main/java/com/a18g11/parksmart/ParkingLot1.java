8f06a844987ef93097ef1876531bfceb1b8dabcf
Jared <jaredping@yahoo.com>
2018-08-01 20:37:46 +0200

    Parking lot grid with custom cell sizing + colour changeable cells

---
+++ Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLot1.java
@@ -0,0 +1,87 @@
+package com.a18g11.parksmart;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.group18g11.parksmart.R;
+
+public class ParkingLot1 extends AppCompatActivity {
+
+    private Context mContext;
+    private Activity mActivity;
+    private RelativeLayout mCLayout;
+
+    GridView gridView;
+    private String[] parkingNumbers = new String[30*13];
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_lot1);
+
+        // Get the application context
+        mContext = getApplicationContext();
+        mActivity = ParkingLot1.this;
+        mCLayout = (RelativeLayout) findViewById(R.id.relative_layout_parkings);
+
+        for(int index = 0; index < parkingNumbers.length; index++){
+            parkingNumbers[index] = "_";
+        }
+
+        gridView = (GridView) findViewById(R.id.gridview_parkings);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, parkingNumbers){
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent){
+                // Cast the grid view current item as a text view
+                TextView tv_cell = (TextView) super.getView(position,convertView,parent);
+
+                // Set the item background drawable
+                if(position != 7) {
+                    tv_cell.setBackground(
+                            ContextCompat.getDrawable(
+                                    mContext, R.drawable.gridview_item_background
+                            )
+                    );
+                }
+
+                // Put item item text in cell center
+                tv_cell.setGravity(Gravity.CENTER);
+
+                /*
+                    void setHeight (int pixels)
+                        Makes the TextView exactly this many pixels tall. You could do the same
+                        thing by specifying this number in the LayoutParams. Note that
+                        setting this value overrides any other
+                        (minimum / maximum) number of lines or height setting.
+
+                        Parameters
+                            pixels : int
+                */
+
+                // Set the grid view item/cell/row height
+                //tv_cell.setHeight(300); // In pixels
+
+                // Another way to change grid view row height
+                tv_cell.getLayoutParams().height = 100;
+                tv_cell.getLayoutParams().width = 60;
+
+                // Return the modified item
+                return tv_cell;
+            };
+        };
+        gridView.setAdapter(adapter);
+    }
+}
--- Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLotsListActivity.java
+++ Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLotsListActivity.java
@@ -1,5 +1,7 @@
 package com.a18g11.parksmart;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -14,6 +16,12 @@ import java.util.ArrayList;
 
 public class ParkingLotsListActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
+    public static final int LOT_1 = 0;
+    public static final int LOT_2 = 1;
+    public static final int LOT_3 = 2;
+    public static final int LOT_4 = 3;
+    public static final int LOT_5 = 4;
+
     MyRecyclerViewAdapter adapter;
 
     @Override
@@ -22,20 +30,21 @@ public class ParkingLotsListActivity extends AppCompatActivity implements MyRecy
         setContentView(R.layout.activity_parking_lots_list);
 
         // data to populate the RecyclerView with
-        ArrayList<String> animalNames = new ArrayList<>();
-        animalNames.add("Parking Lot 1 \t Value");
-        animalNames.add("Parking Lot 2 \t Value");
-        animalNames.add("Parking Lot 3 \t Value");
-        animalNames.add("Parking Lot 4 \t Value");
-        animalNames.add("Parking Lot 5 \t Value");
+        ArrayList<String> parkingLots = new ArrayList<>();
+        parkingLots.add("Parking Lot 1 \t Value");
+        parkingLots.add("Parking Lot 2 \t Value");
+        parkingLots.add("Parking Lot 3 \t Value");
+        parkingLots.add("Parking Lot 4 \t Value");
+        parkingLots.add("Parking Lot 5 \t Value");
 
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.parking_lots);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter = new MyRecyclerViewAdapter(this, parkingLots);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
 
+        // vertical list dividers for RecyclerView
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
         recyclerView.addItemDecoration(decoration);
     }
@@ -43,5 +52,23 @@ public class ParkingLotsListActivity extends AppCompatActivity implements MyRecy
     @Override
     public void onItemClick(View view, int position) {
         Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        Context context = view.getContext();
+
+        switch(position){
+            case LOT_1:
+                Intent intent = new Intent(context, ParkingLot1.class);
+                context.startActivity(intent);
+                break;
+            case LOT_2:
+                break;
+            case LOT_3:
+                break;
+            case LOT_4:
+                break;
+            case LOT_5:
+                break;
+            default:
+                break;
+        }
     }
 }
