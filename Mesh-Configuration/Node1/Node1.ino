e9a59ab2131270be4dec5edecb3e3076e7131577
Jared <jaredping@yahoo.com>
2018-08-14 15:58:13 +0200

    Sensor node in row

---
+++ Mesh-Configuration/Node1/Node1.ino
@@ -0,0 +1,56 @@
+#include <avr/power.h>
+#include <SPI.h> 
+#include "RF24.h" 
+#include "RF24Network.h"
+#include "LowPower.h"
+
+RF24 myRadio (A0, 10); 
+RF24Network network(myRadio);
+int dataTransmitted;
+bool returned_results;
+
+// Octal Mapping
+const uint16_t this_node = 011;
+const uint16_t row_admin = 01;
+
+void setup() {
+  clock_prescale_set(clock_div_16);
+  Serial.begin(9600);
+  Serial.println(F("Sensor Node - Node1")); 
+  SPI.begin();
+  myRadio.begin();
+  network.begin(90, this_node); //(channel, node address)
+  myRadio.setDataRate(RF24_250KBPS);
+  myRadio.setPALevel(RF24_PA_MIN); 
+  dataTransmitted = 100; 
+}
+void loop() {
+  returned_results = false;
+
+  for(int i = 0; i < 4; i++){
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+
+  while(!returned_results){
+    network.update();
+    //===== Receiving =====//
+    while ( network.available() ) {     // Is there any incoming data?
+      RF24NetworkHeader header;
+      unsigned long incomingData = 0;
+      network.read(header, &incomingData, sizeof(incomingData)); // Read the incoming data
+      Serial.print("Data received: ");
+      Serial.println(incomingData);
+      if (header.from_node == 01) {    // If data comes from Node 4
+        RF24NetworkHeader header2(row_admin);     // (Address where the data is going)
+  
+        bool ok = network.write(header2, &dataTransmitted, sizeof(dataTransmitted)); // Send the data
+        Serial.print("Data transmitted: ");
+        Serial.println(dataTransmitted);
+        dataTransmitted++;
+        returned_results = true;
+      }
+    }
+  }
+}
+
+
