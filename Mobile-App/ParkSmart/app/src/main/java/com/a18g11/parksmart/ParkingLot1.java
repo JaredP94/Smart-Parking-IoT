d6a724425f0221c906b066502cf2130bfc7ad1c4
Jared <jaredping@yahoo.com>
2018-08-23 20:30:33 +0200

    Auto update data query for parking lot

--- Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLot1.java
+++ Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/ParkingLot1.java
@@ -15,9 +15,25 @@ import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.group18g11.parksmart.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ParkingLot1 extends AppCompatActivity {
 
@@ -34,11 +50,28 @@ public class ParkingLot1 extends AppCompatActivity {
             296, 297, 324, 325, 298, 299, 326, 327, 300, 301, 328, 329, 302, 303, 330, 331, 304, 305, 332, 333, 306, 307, 334, 335
         };
     private ArrayList<Character> parkingOccupancy = new ArrayList<>();
+    static final int update_time = 30000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lot1);
+
+        final RequestQueue mRequestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        mRequestQueue.start();
+
+        final String url = "https://api.thingspeak.com/channels/542645/fields/2&3&4.json?api_key=GX2QFYAFHEZ86AIO&results=1&timezone=Africa%2FJohannesburg";
 
         // Get the application context
         mContext = getApplicationContext();
@@ -75,7 +108,6 @@ public class ParkingLot1 extends AppCompatActivity {
                 parkingOccupancy.add(parking_bays[i][j]);
             }
         }
-
 
         for(int index : trackedParkings){
             parkingNumbers[index] = parkingOccupancy.get(track_index).toString();
@@ -127,8 +159,6 @@ public class ParkingLot1 extends AppCompatActivity {
                     );
                 }
 
-
-
                 // Put item item text in cell center
                 if(position == 25) {
                     tv_cell.setGravity(Gravity.TOP);
@@ -165,6 +195,153 @@ public class ParkingLot1 extends AppCompatActivity {
                 return tv_cell;
             }
         };
+
         gridView.setAdapter(adapter);
+
+        Timer t = new Timer();
+
+        //Set the schedule function and rate
+        t.scheduleAtFixedRate(new TimerTask() {
+          @Override
+          public void run() {
+              Log.i("Timer", "Updated");
+              JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                  (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                      @Override
+                      public void onResponse(JSONObject response) {
+                          Log.i("API Resp ", response.toString());
+                          //data[0] = response.getJSONObject("channel");
+                          try {
+                              JSONArray feeds = response.getJSONArray("feeds");
+                              JSONObject latest_feed = feeds.getJSONObject(0);
+                              parking_bays[0] = latest_feed.get("field2").toString().toCharArray();
+                              parking_bays[1] = latest_feed.get("field3").toString().toCharArray();
+                              parking_bays[2] = latest_feed.get("field4").toString().toCharArray();
+
+                              Log.i("Feed ", feeds.toString());
+                              Log.i("Latest Feed ", latest_feed.get("field2").toString());
+                              Log.i("Parking Bays 1 ", String.valueOf(parking_bays[0]));
+                              Log.i("Parking Bays 2 ", String.valueOf(parking_bays[1]));
+                              Log.i("Parking Bays 3 ", String.valueOf(parking_bays[2]));
+
+                              int track_index = 0;
+
+                              for (int i = 0; i < 3; i++){
+                                  for (int j = 0; j < 24; j++){
+                                      parkingOccupancy.add(parking_bays[i][j]);
+                                  }
+                              }
+
+                              for(int index : trackedParkings){
+                                  parkingNumbers[index] = parkingOccupancy.get(track_index).toString();
+                                  track_index++;
+                              }
+
+                              ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, parkingNumbers){
+                                  @Override
+                                  public View getView(int position, View convertView, ViewGroup parent){
+                                      // Cast the grid view current item as a text view
+                                      TextView tv_cell = (TextView) super.getView(position,convertView,parent);
+
+                                      if(position == 25){
+                                          tv_cell.setTextColor(Color.BLACK);
+                                          tv_cell.setTextSize(20);
+                                          tv_cell.setText("Entrance");
+                                      }
+
+                                      // Set the item background drawable
+                                      if(parkingNumbers[position].equals("3")) {
+                                          tv_cell.setBackground(
+                                                  ContextCompat.getDrawable(
+                                                          mContext, R.drawable.gridview_item_background_uncovered_parkings
+                                                  )
+                                          );
+                                      }
+                                      else if(parkingNumbers[position].equals("1")) {
+                                          tv_cell.setBackground(
+                                                  ContextCompat.getDrawable(
+                                                          mContext, R.drawable.gridview_item_background_occupied_parking
+                                                  )
+                                          );
+                                      }
+
+                                      else if(parkingNumbers[position].equals("0")) {
+                                          tv_cell.setBackground(
+                                                  ContextCompat.getDrawable(
+                                                          mContext, R.drawable.gridview_item_background_unoccupied_parking
+                                                  )
+                                          );
+                                      }
+
+                                      else if(parkingNumbers[position].equals("2")) {
+                                          tv_cell.setBackground(
+                                                  ContextCompat.getDrawable(
+                                                          mContext, R.drawable.gridview_item_background_node_error
+                                                  )
+                                          );
+                                      }
+
+                                      // Put item item text in cell center
+                                      if(position == 25) {
+                                          tv_cell.setGravity(Gravity.TOP);
+                                      }
+                                      else {
+                                          tv_cell.setGravity(Gravity.CENTER);
+                                      }
+
+                                        /*
+                                            void setHeight (int pixels)
+                                                Makes the TextView exactly this many pixels tall. You could do the same
+                                                thing by specifying this number in the LayoutParams. Note that
+                                                setting this value overrides any other
+                                                (minimum / maximum) number of lines or height setting.
+
+                                                Parameters
+                                                    pixels : int
+                                        */
+
+                                      // Set the grid view item/cell/row height
+                                      //tv_cell.setHeight(300); // In pixels
+
+                                      // Another way to change grid view row height
+                                      if(position == 25) {
+                                          tv_cell.getLayoutParams().height = 100;
+                                          tv_cell.getLayoutParams().width = 350;
+                                      }
+                                      else{
+                                          tv_cell.getLayoutParams().height = 71;
+                                          tv_cell.getLayoutParams().width = 65;
+                                      }
+
+                                      // Return the modified item
+                                      return tv_cell;
+                                  }
+                              };
+
+                          adapter.notifyDataSetChanged();
+                          gridView.setAdapter(adapter);
+
+                          } catch (JSONException e) {
+                              e.printStackTrace();
+                          }
+                      }
+                  }, new Response.ErrorListener() {
+
+                      @Override
+                      public void onErrorResponse(VolleyError error) {
+                          // TODO: Handle error
+
+                      }
+                  });
+
+              // Add the request to the RequestQueue.
+              mRequestQueue.add(jsonObjectRequest);
+          }
+      },
+    //Set how long before to start calling the TimerTask (in milliseconds)
+    0,
+    //Set the amount of time between each execution (in milliseconds)
+    update_time);
     }
 }
