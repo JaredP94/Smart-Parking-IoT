e3b52f9afd321b396137f7235fd170a78e127595
Jared <jaredping@yahoo.com>
2018-08-24 10:35:54 +0200

    Reconfigure Node3

--- Mesh-Configuration/Node3/Node3.ino
+++ Mesh-Configuration/Node3/Node3.ino
@@ -5,34 +5,49 @@
 #include "LowPower.h"
 
 #define NUM_SENSORS 4
+#define WAIT_TIME 10
 
 RF24 myRadio (A0, 10); 
 RF24Network network(myRadio);
-int dataTransmitted;
 bool returned_results;
-
-signed char parkingsTracked[NUM_SENSORS] = {10, 11, 12, 13};
-int isOccupied[NUM_SENSORS] = {0, 0, 0, 0};
 
 // Octal Mapping
 const uint16_t this_node = 031;
 const uint16_t row_admin = 01;
 
+// pin setup
+const int trigger[NUM_SENSORS] = {2, 4, 6, 8};
+const int echo[NUM_SENSORS] = {3, 5, 7, 9};
+
+float timeoutDist;
+float timeout;
+
 void setup() {
   clock_prescale_set(clock_div_16);
   Serial.begin(9600);
-  Serial.println(F("Sensor Node - Node3")); 
+  Serial.println(F("Sensor Node - Node2")); 
+  
   SPI.begin();
   myRadio.begin();
   network.begin(90, this_node); //(channel, node address)
   myRadio.setDataRate(RF24_250KBPS);
   myRadio.setPALevel(RF24_PA_MIN); 
-  dataTransmitted = 100; 
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    pinMode(trigger[i], OUTPUT);
+    pinMode(echo[i], INPUT);
+  }
+
+  pinMode(A5, OUTPUT);
+
+  timeoutDist = 200;            //200cm
+  timeout = timeoutDist * 58;   //corresponding time inmicroSecond
 }
 void loop() {
-  for(int i = 0; i < 4; i++){
+  for(int i = 0; i < 3; i++){
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
+  LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
 
   returned_results = false;
 
@@ -46,16 +61,60 @@ void loop() {
       Serial.print("Data received: ");
       Serial.println(incomingData);
       if (header.from_node == row_admin) {    // If data comes from Node 4
-        RF24NetworkHeader header2(row_admin);     // (Address where the data is going)
-  
-        bool ok = network.write(header2, &parkingsTracked, sizeof(parkingsTracked)); // Send the data
+        char isOccupied[NUM_SENSORS] = {0, 0, 0, 0};
+        float distance[NUM_SENSORS] = {0, 0, 0, 0};
+        
+        digitalWrite(A5, HIGH);
+        
+        for (int i = 0; i < NUM_SENSORS; i++) {
+          distance[i] =  getDistance(trigger[i], echo[i]);
+          isOccupied[i] = checkOccupied(distance[i]);
+          Serial.print("Distance: ");
+          Serial.print(distance[i]);
+          Serial.print(" cm \t");
+          Serial.print("status:");
+          Serial.println(isOccupied[i]);
+          delay(WAIT_TIME);
+        }
+
+        digitalWrite(A5, LOW);
+        
+        bool ok = false;
+        while (!ok){
+          Serial.println(F("Attempt transmission to row admin"));
+          RF24NetworkHeader header2(row_admin);     // (Address where the data is going)
+          ok = network.write(header2, &isOccupied, sizeof(isOccupied)); // Send the data
+          delay(10);
+        }
         Serial.print("Data transmitted to row admin: ");
-        //Serial.println(dataTransmitted);
-        //dataTransmitted++;
         returned_results = true;
       }
     }
   }
+
+  Serial.flush();
+}
+
+float getDistance(const int trigPin, const int echoPin){
+  float distance, duration;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH, timeout);
+  distance = duration/58;
+  return distance;
+}
+
+int checkOccupied(float distance) {
+  int occupancyStatus;
+  if (distance > 0 && distance <= timeoutDist){
+    occupancyStatus = 1;
+  } else {
+    occupancyStatus = 0;
+  }
+  return occupancyStatus;
 }
 
 
