bcca56e3d3972c52f2a038bba3d1373bb05cb35d
Jared <jaredping@yahoo.com>
2018-08-26 02:32:30 +0200

    Reconfigure Node 1

--- Mesh-Configuration/Node1/Node1.ino
+++ Mesh-Configuration/Node1/Node1.ino
@@ -35,7 +35,7 @@ void setup() {
   myRadio.begin();
   network.begin(90, this_node); //(channel, node address)
   myRadio.setDataRate(RF24_250KBPS);
-  myRadio.setPALevel(RF24_PA_MIN); 
+  myRadio.setPALevel(RF24_PA_HIGH); 
 
   for (int i = 0; i < NUM_SENSORS; i++) {
     pinMode(trigger[i], OUTPUT);
@@ -53,7 +53,7 @@ void loop() {
   }
   LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
   LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
-  LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
+  LowPower.powerDown(SLEEP_250MS, ADC_OFF, BOD_OFF);
 
   Serial.print("on");
   total_on_time = micros();
