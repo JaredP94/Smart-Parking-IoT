7599bce70b1a8b8f8792b7864a63b0f35204ea30
Jared <jaredping@yahoo.com>
2018-08-26 04:00:48 +0200

    Improved splash screen

--- Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/SplashScreenActivity.java
+++ Mobile-App/ParkSmart/app/src/main/java/com/a18g11/parksmart/SplashScreenActivity.java
@@ -37,6 +37,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
 
         RequestQueue mRequestQueue;
 
